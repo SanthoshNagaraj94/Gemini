9cf7199a28fece080f10ffd7d3b66b1063a6be95
SANTHOSH NAGARAJ <85549725+SanthoshNagaraj94@users.noreply.github.com>
2023-12-21 03:07:40 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import os
 from pymongo import MongoClient
 import base64
 import re
+#datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+from datetime import datetime
 
 client=MongoClient("mongodb+srv://santhosh:sam123@cluster0.mvgwd.mongodb.net/?retryWrites=true&w=majority")
 db=client['Guvi']
@@ -32,7 +34,7 @@ elif Side_option=="Image Q&A":
         st.markdown(response.text)
         # save the image, prompt input and response to upload into mongoDB
         Image=open("image.jpeg","rb").read()
-        coll.insert_one({"image":base64.b64encode(Image),"prompt":Prompt,"response":response.text})
+        coll.insert_one({"Date":datetime.now().strftime("%Y-%m-%d %H:%M:%S"),"SystemName":os.getlogin(),"image":base64.b64encode(Image),"prompt":Prompt,"response":response.text})
 
         os.remove("image.jpeg")
 
@@ -53,6 +55,6 @@ elif Side_option=="Image Description":
         
         st.markdown(response.text)
         Image=open("image.jpeg","rb").read()
-        coll.insert_one({"image":base64.b64encode(Image),"prompt":Prompt,"response":response.text})
+        coll.insert_one({"Date":datetime.now().strftime("%Y-%m-%d %H:%M:%S"),"SystemName":os.getlogin(),"image":base64.b64encode(Image),"prompt":Prompt,"response":response.text})
         
         os.remove("image.jpeg")
